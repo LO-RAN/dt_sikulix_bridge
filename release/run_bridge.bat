057a6e4b4274eb6c16f7a446e9da05b6e66e2201
Laurent IZAC <laurent.izac@dynatrace.com>
2021-12-20 11:47:20 +0100

    tried to make it more independent from the file system context

--- release/run_bridge.bat
+++ release/run_bridge.bat
@@ -1,12 +1,13 @@
 @echo off
 echo adding the path to the Java runtime included in the Dynatrace ActiveGate distribution
 set PATH="c:\Program Files\dynatrace\gateway\jre\bin";%PATH%
-
+set JAVA_HOME=c:\Program Files\dynatrace\gateway\jre
 
 rem default is to use SikuliX if no parameter is provided on the command line
 rem note: first placeholder is the script name
 rem       second placeholder is the function to execute
-set CMD="java -jar sikulixide-2.0.5.jar -r {} --args \"{}\""
+
+set CMD="\"%JAVA_HOME%\bin\java\" -jar \"%~dp0sikulixide-2.0.5.jar\" -r {} --args \"{}\""
 rem  but can be used also with other tools, like AutoIt : 
 rem  set CMD="AutoIt3.exe {} {}"
 
