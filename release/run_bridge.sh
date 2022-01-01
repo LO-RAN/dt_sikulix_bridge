e4cd3cc6e59f470682106a66e80ca8a35883d232
Laurent IZAC <laurent.izac@dynatrace.com>
2022-01-01 20:52:25 +0100

    fixed SikuliX command line

--- release/run_bridge.sh
+++ release/run_bridge.sh
@@ -8,7 +8,7 @@ export JAVA_HOME=/opt/dynatrace/gateway/jre
 # note: first placeholder is the script name
 #       second placeholder is the function to execute
 
-export CMD="$JAVA_HOME%/bin/java -jar $PWD/sikulixide-2.0.5.jar -r {} --args \"{}\""
+export CMD="$JAVA_HOME/bin/java -jar $PWD/sikulixide-2.0.5.jar -r {} --args '{}'"
 
 # where to generate timings and screenshots upon script execution
 export DT_BRIDGE_OUTPUT=/tmp
