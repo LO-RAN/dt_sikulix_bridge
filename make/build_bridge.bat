ae5bfebe5034fcf5153301cb12aa737c08b56c76
Laurent IZAC <laurent.izac@dynatrace.com>
2022-02-03 20:59:49 +0100

    removed harming double quotes (moved them to command line generation in Python code)

--- make/build_bridge.bat
+++ make/build_bridge.bat
@@ -3,6 +3,8 @@ pyinstaller -F --add-data "templates;templates" --add-data "static;static"  --di
 rem copy python utilities (timing decorator and vnc connection helpers)
 rem Note: see src/README_dtbridge_sikulix.txt for how this jar file was creaed
 copy dtbridge_sikuli.jar ..\release\
+rem copy SSL certificate
+copy *.pem ..\release\
 rem copy SikuliX jar from official web site (as it is too big to be commited to GIT)
 powershell (new-object System.Net.WebClient).DownloadFile('https://launchpad.net/sikuli/sikulix/2.0.5/+download/sikulixide-2.0.5.jar','..\release\sikulixide-2.0.5.jar')
 cd ..\make
--- make/build_bridge.sh
+++ make/build_bridge.sh
@@ -5,6 +5,8 @@ pyinstaller -F --add-data "templates:templates" --add-data "static:static"  --di
 # copy python utilities (timing decorator and vnc connection helpers)
 # Note: see src/README_dtbridge_sikulix.txt for how this jar file was creaed
 copy $PWD/dtbridge_sikuli.jar ../release/
+# copy SSL certificate
+copy $PWD/*.pem ../release/
 # copy SikuliX jar from official web site (as it is too big to be commited to GIT)
 cp $PWD/dtbridge_sikuli.jar ../release/
 wget -O ../release/sikulixide-2.0.5.jar https://launchpad.net/sikuli/sikulix/2.0.5/+download/sikulixide-2.0.5.jar
